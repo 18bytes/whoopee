2dddac6fa39eb59e0fce89d834ca0939cff34208
Sundar <mail@devsundar.com>
2011-06-20 20:02:11 +0200

    read ini file from Python

--- python/readini/readconfig.py
+++ python/readini/readconfig.py
@@ -1,7 +1,7 @@
 import ConfigParser
 
 config = ConfigParser.ConfigParser()
-config.read('/home/meenu/ws/hg/learn/python/config.ini')
+config.read('/home/meenu/ws/hg/learn/python/readini/config.ini')
 
 print "Section read from ini file:" + config.get('isection', 'testparam')
 
