aa6fe4521d7c01ab5c823a2007b061958b618386
Sundar <mail@devsundar.com>
2011-12-17 15:10:15 +0100

    commiting old files

--- learning/python/lpthw/ex3.py
+++ learning/python/lpthw/ex3.py
@@ -5,8 +5,7 @@ print "Roosters", 100 - 25 * 3 % 4
 
 print "Now I will count the eggs:"
 
-print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 
-+ 6
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
 
 print "Is it true that 3 + 2 < 5 - 7?"
 print 3 + 2 < 5 - 7
