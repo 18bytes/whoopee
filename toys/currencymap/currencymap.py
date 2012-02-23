c93867fab31f432340f3fd017d8265e5e6dbe937
Sundar <mail@devsundar.com>
2012-02-23 12:09:46 +0100

    Improved currency map

--- toys/currencymap/currencymap.py
+++ toys/currencymap/currencymap.py
@@ -1,8 +1,6 @@
 import webapp2
-import os
-import jinja2
 from google.appengine.api import urlfetch
-import json
+import json, jinja2, os
 import config
 
 jinja_environment = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
@@ -12,10 +10,19 @@ class MainPage(webapp2.RequestHandler):
   def get(self):
       resp = urlfetch.fetch(url)
       data = json.loads(resp.content)
+      base = self.request.get("base")
+      base = "INR"
+      factor =  1 / (data['rates'][base])
+      for code in data['rates']:
+        data['rates'][code] = 1 / (data['rates'][code] * factor)
+
       template_values = {
+        'base': base,
+        'rates': data['rates'],
+        'countries': config.countries,
       }
       template = jinja_environment.get_template('index.html')
+      #self.response.out.write(factor)
       self.response.out.write(template.render(template_values))
 
-app = webapp2.WSGIApplication([('/', MainPage)],
-                              debug=True)
+app = webapp2.WSGIApplication([('/', MainPage)], debug=True)
