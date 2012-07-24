84dba519a59d15e2fe242415b9af36bb97194d79
Sundar <mail@devsundar.com>
2012-07-24 17:04:12 +0530

    Updates

---
+++ problems/projectEuler/scala/p7.scala
@@ -0,0 +1,13 @@
+object FindPrime {
+
+  def main(args: Array[String]): Unit = {
+    isPrime(22)
+  }
+
+  def isPrime(x: Int): Boolean = {
+    var status = false
+    for (i:int ;)
+    status
+
+  }
+}
